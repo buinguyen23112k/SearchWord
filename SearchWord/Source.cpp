4eecfd5fad7196e927f48c145af4d43fcea9cfd4
buinguyen2311 <buinguyen2311@gmail.com>
2020-11-25 19:48:23 +0700

    initialize file

---
+++ SearchWord/Source.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <string>
+int main()
+{
+    _setmode(_fileno(stdin), _O_WTEXT);
+    _setmode(_fileno(stdout), _O_WTEXT);
+    std::wcout << L"Tiếng Việt có dấu" << std::endl;
+    wchar_t test[256] = { 0 };
+    
+    std::wcin >> test;
+    return 0;
+}
